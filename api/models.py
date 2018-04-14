dec2f63ead86811c915408874e4a842aeb78312f
Dmitry <barskoidim@gmail.com>
2018-04-14 10:30:44 +0300

    date field added to model record

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
+import datetime
+
+from django.utils import timezone
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -15,6 +19,10 @@ class Record(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     score = models.IntegerField(default=0)
+    date = models.DateTimeField('date played', default=timezone.now())
+
+    def played_recently(self):
+        return self.date >= timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
         return ' '.join(str(self.game) + str(self.user) + str(self.score))
--- api/urls.py
+++ api/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 from api import views
-
 urlpatterns = [
     # pass
 ]
