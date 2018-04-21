99424d85289ac6c7236662949a90617d4c42f3af
Dmitry <barskoidim@gmail.com>
2018-04-21 14:01:35 +0300

    webv is removed

--- webv/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebvConfig(AppConfig):
-    name = 'webv'
--- webv/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from webv.views import show_r
-
-urlpatterns = [
-    url(r'^', show_r)
-]
-
