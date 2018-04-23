c90ade176a2a94f364c518f8230eebb123cbd057
flisoch <flisochcs@gmail.com>
2018-04-23 18:18:59 +0300

    JWT settings added(expiration time prolonged

--- tr/settings.py
+++ tr/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
+import datetime
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -143,3 +143,17 @@ STATIC_URL = '/static/'
 
 
 LOGIN_URL = 'login'
+
+JWT_AUTH = {
+    # how long the original token is valid for
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=14),
+
+    # # allow refreshing of tokens
+    # 'JWT_ALLOW_REFRESH': True,
+    #
+    # # this is the maximum time AFTER the token was issued that
+    # # it can be refreshed.  exprired tokens can't be refreshed.
+    # 'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+}
+
+
